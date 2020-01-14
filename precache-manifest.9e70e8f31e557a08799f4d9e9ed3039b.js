8d733c89c70e08f084f9f8d7e3cf580986d77b61
Gerard Salisi <gerardsalisi22@gmail.com>
2020-01-13 19:50:22 -0500

    Updates

---
+++ precache-manifest.9e70e8f31e557a08799f4d9e9ed3039b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f51cfbe10cf1819807c416ce8f87f926",
+    "url": "/bee-boys-website/index.html"
+  },
+  {
+    "revision": "fbf786d941c08b3fc536",
+    "url": "/bee-boys-website/static/css/main.6ce043b1.chunk.css"
+  },
+  {
+    "revision": "494c9cf386477ff3bf65",
+    "url": "/bee-boys-website/static/js/2.b41ad24d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/bee-boys-website/static/js/2.b41ad24d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fbf786d941c08b3fc536",
+    "url": "/bee-boys-website/static/js/main.4696bae7.chunk.js"
+  },
+  {
+    "revision": "3bdd16ef5e0b442fff1c",
+    "url": "/bee-boys-website/static/js/runtime-main.d3cec463.js"
+  }
+]);
