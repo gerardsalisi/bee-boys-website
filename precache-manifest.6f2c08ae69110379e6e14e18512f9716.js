54a58da5df6f1edc5f5e802cc28178daa155d598
Gerard Salisi <gerardsalisi22@gmail.com>
2020-01-20 03:12:59 -0500

    Updates

---
+++ precache-manifest.6f2c08ae69110379e6e14e18512f9716.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b44375974fd937189a3deabbb20a4b2a",
+    "url": "/pollimate-website/index.html"
+  },
+  {
+    "revision": "be0f6664706139cf31d3",
+    "url": "/pollimate-website/static/css/main.89d3a9b2.chunk.css"
+  },
+  {
+    "revision": "65bbabbd1cb4be8a9945",
+    "url": "/pollimate-website/static/js/2.82bec024.chunk.js"
+  },
+  {
+    "revision": "52da1c80261f94121da7e4956cd11404",
+    "url": "/pollimate-website/static/js/2.82bec024.chunk.js.LICENSE"
+  },
+  {
+    "revision": "be0f6664706139cf31d3",
+    "url": "/pollimate-website/static/js/main.34ecfb03.chunk.js"
+  },
+  {
+    "revision": "c62568fe09efff69e4b9",
+    "url": "/pollimate-website/static/js/runtime-main.263fb788.js"
+  },
+  {
+    "revision": "f47245af086ff9e9c753c2dfe6a66db1",
+    "url": "/pollimate-website/static/media/01-12-20.f47245af.md"
+  },
+  {
+    "revision": "eaff26818253f425fd69be1aac8f7a7b",
+    "url": "/pollimate-website/static/media/01-19-20.eaff2681.md"
+  },
+  {
+    "revision": "026202f9c452f0eb7ff5600d64e9be1b",
+    "url": "/pollimate-website/static/media/bee.026202f9.png"
+  },
+  {
+    "revision": "a675402eef86de58e8e9e71563bf3aa9",
+    "url": "/pollimate-website/static/media/team-photo.a675402e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bee-boys-website/precache-manifest.ad2d1eaf9af067db74c82b43b1ef70c7.js"
+  "/pollimate-website/precache-manifest.6f2c08ae69110379e6e14e18512f9716.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bee-boys-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pollimate-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
